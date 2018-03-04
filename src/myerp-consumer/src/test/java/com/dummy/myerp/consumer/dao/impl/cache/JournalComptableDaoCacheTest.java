679957d10aa016a794ec50bd632b0b71dc85b991
chococrok <moi_viking@hotmail.com>
2018-03-04 18:43:51 +0100

    created some test (not implemented yet) and format one class ctrl+maj+F (annoying) I will try to delete consumer helper. It is useless And remove static method. again annoying for tests..

---
+++ src/myerp-consumer/src/test/java/com/dummy/myerp/consumer/dao/impl/cache/JournalComptableDaoCacheTest.java
@@ -0,0 +1,13 @@
+package com.dummy.myerp.consumer.dao.impl.cache;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class JournalComptableDaoCacheTest {
+
+  @Test
+  public void methodToTest() {
+    Assert.assertTrue(false);
+  }
+
+}
